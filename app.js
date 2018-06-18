3705358f3b8ecf8d87a54e666f4ced11e2018bd5
Ninh Le <levanninh0101@gmail.com>
2018-06-18 10:08:42 +0700

    try to change webhook url

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const messengerSettings = {
       pageToken: 'EAAPE9g5H2FABAALaTDtoMaxhrZBX6COediOLoWDIeZCb3q5eu4HjMwuLzZC9SeAChUshEoO2ZB8z5mUtRZCzxX80IJ7pZAbXkyNjL2b73rgLZA6iZCIoqg93EDkCRR59cjQDSsj8d07TUtk6CkZAkfsr8HRkGmaBxiXDwoWRwR9vXgAZDZD',
       fbAppSecret: '6bb28e299667852d628a91ef66a17518',
     },
-    webhookEndpoint: 'webhook', // botmaster will mount this webhook on https://Your_Domain_Name/messenger/webhook
+    webhookEndpoint: 'webhook1234', // botmaster will mount this webhook on https://Your_Domain_Name/messenger/webhook1234
   };
 
   const messengerBot = new MessengerBot(messengerSettings);
